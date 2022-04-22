f9806578e258754fc1a44257f86d82e319a81244
Aleksandr <elchaninov79@gmail.com>
2022-04-22 20:51:50 +0300

    I switched to the OkHttpClient library to work with WebSocket.

--- app/src/main/java/com/elchaninov/espmanager/model/ms/MsPage.kt
+++ app/src/main/java/com/elchaninov/espmanager/model/ms/MsPage.kt
@@ -1,6 +1,6 @@
 package com.elchaninov.espmanager.model.ms
 
 enum class MsPage(val path: String) {
-    INDEX("/index.htm"),
-    SETUP("/setup.htm")
+    INDEX("index.htm"),
+    SETUP("setup.htm")
 }
--- app/src/main/java/com/elchaninov/espmanager/view/ms/ViewModelFragmentMsMain.kt
+++ app/src/main/java/com/elchaninov/espmanager/view/ms/ViewModelFragmentMsMain.kt
@@ -7,12 +7,7 @@ import androidx.lifecycle.ViewModel
 import com.elchaninov.espmanager.model.DeviceModel
 import com.elchaninov.espmanager.model.ms.*
 import com.google.gson.Gson
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.websocket.*
-import io.ktor.http.*
-import io.ktor.websocket.*
-import kotlinx.coroutines.*
+import okhttp3.*
 
 
 open class ViewModelFragmentMsMain(
@@ -20,91 +15,61 @@ open class ViewModelFragmentMsMain(
     private val msPage: MsPage
 ) : ViewModel() {
 
-    private var httpClient: HttpClient? = null
-    private var messageToServer: MsModelForSend? = null
+    private val httpClient = OkHttpClient()
+    private lateinit var request: Request
+    private lateinit var webSocketListener: WebSocketListener
+    private var mWebSocket: WebSocket? = null
     private val gson = Gson()
-
-    private var webSocketsJob: Job? = null
-    private val viewModelCoroutineScope = CoroutineScope(
-        Dispatchers.IO
-                + SupervisorJob()
-                + CoroutineExceptionHandler { _, throwable ->
-            handleError(throwable)
-        })
 
     private val _liveData: MutableLiveData<MsModel> = MutableLiveData()
     val liveData: LiveData<MsModel> get() = _liveData
 
     fun send(msModelForSend: MsModelForSend) {
-        messageToServer = msModelForSend
+        toLog("Отправляем: ${gson.toJson(msModelForSend)}")
+        mWebSocket?.send(gson.toJson(msModelForSend))
+    }
+
+    init {
+        toLog("INIT")
+        deviceModel.ip?.let { ip ->
+            request = Request.Builder().url("ws://$ip:${deviceModel.port}/${msPage.path}").build()
+            toLog("INIT OkHttpClient, request=$request")
+
+            webSocketListener = object : WebSocketListener() {
+                override fun onOpen(webSocket: WebSocket, response: Response) {
+                    super.onOpen(webSocket, response)
+                    toLog("Соединение с WebSocket установлено")
+                    mWebSocket = webSocket
+                }
+
+                override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+                    super.onClosed(webSocket, code, reason)
+                    toLog("Соединение с WebSocket закрыто")
+                }
+
+                override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                    super.onFailure(webSocket, t, response)
+                    toLog("Ошибка: ${t.message}")
+                    t.printStackTrace()
+                }
+
+                override fun onMessage(webSocket: WebSocket, text: String) {
+                    super.onMessage(webSocket, text)
+                    _liveData.postValue(deserializationJson(text))
+                    toLog("onMessage: $text")
+                }
+            }
+        }
     }
 
     fun start() {
+        httpClient.newWebSocket(request, webSocketListener)
         toLog("start()")
-        deviceModel.ip?.let { ip ->
-            if (httpClient == null) {
-                httpClient = HttpClient(CIO) {
-                    install(WebSockets)
-                }
-
-                webSocketsJob = viewModelCoroutineScope.launch { startWebSockets(ip) }
-                toLog("start() init httpClient=$httpClient, init viewModeJob=$webSocketsJob")
-            }
-        }
     }
 
     fun stop() {
-        toLog("stop() httpClient=$httpClient, viewModeJob=$webSocketsJob ")
-        webSocketsJob?.cancel()
-        httpClient?.close()
-        webSocketsJob = null
-        httpClient = null
-    }
-
-    private suspend fun startWebSockets(ip: String) {
-        httpClient?.webSocket(
-            method = HttpMethod.Get,
-            host = ip,
-            port = deviceModel.port,
-            path = msPage.path
-        ) {
-            toLog("httpClient.webSocket init")
-            val userInputRoutine = launch { inputMessages() }
-            val messageOutputRoutine = launch { outputMessages() }
-            userInputRoutine.join()
-            messageOutputRoutine.join()
-        }
-    }
-
-    private suspend fun DefaultClientWebSocketSession.outputMessages() {
-        try {
-            while (true) {
-                (incoming.receive() as? Frame.Text)?.let {
-                    val json = it.readText()
-                    toLog("json: $json")
-                    val msModel: MsModel = deserializationJson(json)
-                    _liveData.postValue(msModel)
-                }
-            }
-        } catch (e: Exception) {
-            println("Error while receiving: " + e.localizedMessage)
-        }
-    }
-
-    private suspend fun DefaultClientWebSocketSession.inputMessages() {
-        while (true) {
-            if (messageToServer != null) {
-                try {
-                    val json = gson.toJson(messageToServer)
-//                    send(json)
-                    messageToServer = null
-                    toLog("Отправлено json: $json")
-                } catch (e: Exception) {
-                    println("Error while sending: " + e.localizedMessage)
-                    return
-                }
-            }
-        }
+        mWebSocket?.close(1000, null)
+        toLog("stop()")
     }
 
     private fun deserializationJson(json: String): MsModel {
@@ -118,14 +83,8 @@ open class ViewModelFragmentMsMain(
         }
     }
 
-    private fun handleError(throwable: Throwable) {
-        toLog("${throwable.message}")
-        throwable.printStackTrace()
-    }
-
     override fun onCleared() {
         toLog("onCleared()")
-        stop()
         super.onCleared()
     }
 
